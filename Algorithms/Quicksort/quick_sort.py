2e54b5002f070444911c49fc9a0ac05889138058
Talko Denis <107505040+TalkoDenis@users.noreply.github.com>
2023-11-22 08:15:30 +0100

    Add files via upload

---
+++ Algorithms/Quicksort/quick_sort.py
@@ -0,0 +1,30 @@
+def partition(nums, low, high):  
+    # В качестве опорного выбирается средний элемент
+    pivot = nums[(low + high) // 2]
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while nums[i] < pivot:
+            i += 1
+
+        j -= 1
+        while nums[j] > pivot:
+            j -= 1
+
+        if i >= j:
+            return j
+
+        # Если элемент с индексом i (слева от опорного) больше, чем
+        # элемент с индексом j (справа от опорного), элемены меняются местами
+        nums[i], nums[j] = nums[j], nums[i]
+
+def quick_sort(nums):  
+    # Вспомогательная функцию, которая вызывается рекурсивно
+    def _quick_sort(items, low, high):
+        if low < high:
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
